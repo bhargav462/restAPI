292e93f4c2e6e51239f9af25d4613f2badb68c49
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-08-25 20:07:39 +0530

    Initializing Api

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+dotenv.config({
+    path: './config/config.env'
+})
+
+app.get('/todo',(req,res) => {
+    console.log("request");
+    res.status(200).send("bhargav");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,
+    console.log(`Server running on port : ${PORT}`.red.underline.bold))
